0361980e652b71d2975f5a66c31d355014974b23
Alexiafern <alexiafernanda718@gmail.com>
2023-03-31 11:34:06 -0300

    modificando atividade001.dart

--- atividade001.dart
+++ atividade001.dart
@@ -1,19 +1,19 @@
 void main(){
-  double nota1 = 7.0;
-  double nota2 = 9.4;
-  double nota3 = 5.0;
-  double nota4 = 6.1;
+  double nota1 = 6.5;
+  double nota2 = 6.5;
+  double nota3 = 6.5;
+  double nota4 = 6.5;
 
   var resultado = (nota1 + nota2 + nota3 + nota4) / 4;
-  
+
   print(resultado);
 
   switch (resultado){
-    case < 3 :
+    case < 4 :
     print("Você foi reprovado");
     break;
 
-    case < 6 :
+    case < 7 :
     print("Você ficou de exame");
     break;
 
